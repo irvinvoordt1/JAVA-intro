a46a022153f110f033eba09c256a22f3234ebf42
Irvin van Oordt <35196855+irvinvoordt1@users.noreply.github.com>
2018-03-05 15:36:59 -0500

    Create TEMPLATES.js

---
+++ TEMPLATES.js
@@ -0,0 +1,32 @@
+ar randomLoc = Math.floor(Math.random()*5);
+var location1 = randomLoc;
+var location2 = location1+1;
+var location3 = location2+1;
+var guess;
+var hit =0;
+var guesses=0;
+var isSunk=false;
+
+while  (isSunk == false) {
+  guess = prompt ("Ready, Set, and FIRE!(Enter a number 0 to 6)");
+  if(guess < 0 || guess > 6){
+    alert("Please enter a valid cell number!");
+  } else {
+      guesses = guesses + 1;
+
+      if ( guess == location1 || guess == location2 || guess == location3 ) {
+
+      alert("HIT!");
+      hit = hit + 1;
+      if (hit == location1,location2,location3) {
+        isSunk = true;
+        alert("You sank my battleship!");
+      }
+      else {
+        alert("MISS");
+      }
+      }
+    }
+}
+var stats= "you took "+ guesses +  "guesses to sink the battleship which means"
+ + " which your shooting accuracy was" + 3/guesses; alert (stats);
